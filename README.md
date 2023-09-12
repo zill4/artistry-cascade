a36f23f4e62f8d9848cb3e3c55078ff821f42b96
Justin Crisp <justin@crispcode.io>
2023-09-12 10:46:20 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Artistry Cascade
+
+[![Build Status](https://travis-ci.org/sjohnsonaz/artistry-cascade.svg?branch=master)](https://travis-ci.org/sjohnsonaz/artistry-cascade) [![npm version](https://badge.fury.io/js/%40artistry%2Fcascade.svg)](https://badge.fury.io/js/%40artistry%2Fcascade)
+
+A powerful component library for Cascade.  All components have been designed with Cascade in mind, reducing re-renders and other JavaScript activity.
+
+Furthermore, the components are designed to work seamlessly together.  They are built using a simple, powerful, and configurable stylesheet.  Most animation is style based, again reducing JavaScript activity.
+
+The API is focused on consistency, and ease of use.
+
+* Button
+* ButtonGroup
+* Carousel
+* Code
+* Form
+* FormContainer
+* FormInput
+* Modal
+* Popover
+* ProgressBar
+* Range
+* Section
+* Tab
+* Toggle
---
+++ webpack.dev.config.js
@@ -0,0 +1,49 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'main': './src/tests/demo/scripts/main.ts',
+        'styles': './src/tests/demo/styles/style.styl'
+    },
+    output: {
+        filename: './bundle/[name].js',
+        libraryTarget: 'var',
+        library: '[name]'
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: ['ts-loader']
+        }, {
+            test: /\.styl$/,
+            //loader: "style-loader!css-loader!clean-css-loader!postcss-loader!less-loader",
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                //{ loader: 'postcss-loader', options: { sourceMap: true } },
+                'stylus-loader'
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'clean-css-loader',
+                //{ loader: 'postcss-loader', options: { sourceMap: true } }
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "bundle/[name].css",
+            chunkFilename: "bundle/[id].css"
+        })
+    ]
+};
